ea07f4012efd7371909b39aca8d57b1114d66163
untitled <ahmedsuleiman940@gmail.com>
2025-06-05 23:34:31 +0100

    working on movie api

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const container = document.getElementById('container')
+
+
+
+function getMovies (){
+    fetch('https://api.themoviedb.org/3/movie/popular?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c')
+    .then(res => res.json())
+    .then(data => {
+        
+        console.log(data.results);
+        data.results.forEach(element => {
+            container.innerHTML += `
+                <div class="movie">
+                <img src="./3d-rendering-person-watching-movie-with-popcorn.jpg" alt="">
+                <p class="movie-title">${element.title}</p>
+                </div>
+            `
+        });
+        
+    })
+}
+
+getMovies()
