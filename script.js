34f2d9f5ba200d0a76d024e120a697354046b499
untitled <ahmedsuleiman940@gmail.com>
2025-06-07 10:06:32 +0100

    added all sections ontop each other

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 const container = document.getElementById('container')
 const headerImage = document.getElementById('headerImage')
 const popular = document.querySelectorAll('.popular')
+const upcoming = document.getElementById('upcoming')
+const topRated = document.getElementById('topRated')
 
 
- popular.forEach(element => {
+//  popular.forEach(element => {
 
-    element.addEventListener('click', ()=> {
-        getMovies(element.value)
-    })
-})
+//     element.addEventListener('click', ()=> {
+//         getMovies(element.value)
+//     })
+// })
 
-function getMovies(value, shouldChange) {
-    fetch(`https://api.themoviedb.org/3/movie/${value}?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+function getMovies(shouldChange) {
+    fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
       .then(res => res.json())
       .then(data => {
         const movies = data.results;
@@ -76,6 +78,158 @@ function getMovies(value, shouldChange) {
         });
       });
   }
+
+
+
+  function upcomingMovies (){
+
+
+    fetch(`https://api.themoviedb.org/3/movie/upcoming?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+    .then(res => res.json())
+    .then(data => {
+        const movies = data.results;
+
+
+
+        $('.owl-carousel').trigger('destroy.owl.carousel');
+      
+
+        movies.forEach(movie => {
+            if (movie.poster_path) {
+              upcoming.innerHTML += `
+                <div class="movie item">
+                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                  <p class="movie-title">${movie.title}</p>
+                </div>
+              `;
+            }else {
+              console.log('dddddddddddddddddddddddddddddddddddd');
+              
+            }
+          });
+
+
+          $('.owl-carousel').owlCarousel({
+            loop: false,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+              0: { items: 1, nav: false },
+              600: { items: 3, nav: false },
+              1000: { items: 5, nav: false }
+            }
+          });
+
+        console.log(data);
+        
+    })
+
+
+  }
+
+
+
+
+
+
+
+  function topRatedMovies (){
+
+
+    fetch(`https://api.themoviedb.org/3/movie/top_rated?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+    .then(res => res.json())
+    .then(data => {
+        const movies = data.results;
+
+
+
+        $('.owl-carousel').trigger('destroy.owl.carousel');
+      
+
+        movies.forEach(movie => {
+            if (movie.poster_path) {
+              topRated.innerHTML += `
+                <div class="movie item">
+                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                  <p class="movie-title">${movie.title}</p>
+                </div>
+              `;
+            }else {
+              console.log('dddddddddddddddddddddddddddddddddddd');
+              
+            }
+          });
+
+
+          $('.owl-carousel').owlCarousel({
+            loop: false,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+              0: { items: 1, nav: false },
+              600: { items: 3, nav: false },
+              1000: { items: 5, nav: false }
+            }
+          });
+
+        console.log(data);
+        
+    })
+
+
+  }
+
+
+
+
+  function nowPlayingMovies (){
+
+
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+    .then(res => res.json())
+    .then(data => {
+        const movies = data.results;
+
+
+
+        $('.owl-carousel').trigger('destroy.owl.carousel');
+      
+
+        movies.forEach(movie => {
+            if (movie.poster_path) {
+              nowPlaying.innerHTML += `
+                <div class="movie item">
+                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                  <p class="movie-title">${movie.title}</p>
+                </div>
+              `;
+            }else {
+              console.log('dddddddddddddddddddddddddddddddddddd');
+              
+            }
+          });
+
+
+          $('.owl-carousel').owlCarousel({
+            loop: false,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+              0: { items: 1, nav: false },
+              600: { items: 3, nav: false },
+              1000: { items: 5, nav: false }
+            }
+          });
+
+        console.log(data);
+        
+    })
+
+
+  }
+
+
+
   
 
 const menuToggle = document.getElementById('menuToggle');
@@ -87,4 +241,8 @@ menuToggle.addEventListener('click', () => {
 });
 
 
-getMovies('popular', 'true')
+
+getMovies('true')
+upcomingMovies()
+topRatedMovies()
+nowPlayingMovies()
