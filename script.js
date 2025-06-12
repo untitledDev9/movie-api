6992e07e114a62b58806fd9432724cf0e4056805
untitled <ahmedsuleiman940@gmail.com>
2025-06-12 08:12:32 +0100

    omo

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
-const container = document.getElementById('container')
+const popular = document.getElementById('popular')
 const headerImage = document.getElementById('headerImage')
-const popular = document.querySelectorAll('.popular')
 const upcoming = document.getElementById('upcoming')
 const topRated = document.getElementById('topRated')
 const searchBtn = document.getElementById('searchBtn')
@@ -121,13 +120,31 @@ function getMovies(shouldChange) {
         }
   
 
+      
+
+      });
+  }  getMovies('true')
+
+
+
+
+
+  function popularMovies (){
+
+
+    fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+    .then(res => res.json())
+    .then(data => {
+        const movies = data.results;
+
+
         $('.owl-carousel').trigger('destroy.owl.carousel');
-        $('.owl-carousel').html(''); 
+        // $('.owl-carousel').html(''); 
   
 
         movies.forEach(movie => {
           if (movie.poster_path) {
-            container.innerHTML += `
+            popular.innerHTML += `
               <div class="movie item">
                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
               </div>
@@ -142,164 +159,168 @@ function getMovies(shouldChange) {
         $('.owl-carousel').owlCarousel({
           loop: true,
           margin: 10,
-          responsiveClass: true,
+          responsiveClass: false,
           responsive: {
             0: { items: 2.5, nav: false },
             600: { items: 3, nav: false },
             1000: { items: 5, nav: false }
           }
         });
+        
+    })
 
-      });
-  }  getMovies('true')
+
+  }  popularMovies()
+
+
 
 
 
   
-  function upcomingMovies (){
+  // function upcomingMovies (){
 
 
-    fetch(`https://api.themoviedb.org/3/movie/upcoming?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
-    .then(res => res.json())
-    .then(data => {
-        const movies = data.results;
+  //   fetch(`https://api.themoviedb.org/3/movie/upcoming?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //       const movies = data.results;
         
-        // mainMoviesStorage = [...mainMoviesStorage, movies]
-        // console.log(mainMoviesStorage);
+  //       // mainMoviesStorage = [...mainMoviesStorage, movies]
+  //       // console.log(mainMoviesStorage);
 
 
 
-        $('.owl-carousel').trigger('destroy.owl.carousel');
+  //       $('.owl-carousel').trigger('destroy.owl.carousel');
       
 
-        movies.forEach(movie => {
-            if (movie.poster_path) {
-              upcoming.innerHTML += `
-                <div class="movie item">
-                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-                </div>
-              `;
-            }else {
-              console.log('dddddddddddddddddddddddddddddddddddd');
+  //       movies.forEach(movie => {
+  //           if (movie.poster_path) {
+  //             upcoming.innerHTML += `
+  //               <div class="movie item">
+  //                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+  //               </div>
+  //             `;
+  //           }else {
+  //             console.log('dddddddddddddddddddddddddddddddddddd');
               
-            }
-          });
+  //           }
+  //         });
 
 
-          $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            responsiveClass: true,
-            responsive: {
-              0: { items: 2.5, nav: false },
-              600: { items: 3, nav: false },
-              1000: { items: 5, nav: false }
-            }
-          });
+  //         $('.owl-carousel').owlCarousel({
+  //           loop: true,
+  //           margin: 10,
+  //           responsiveClass: true,
+  //           responsive: {
+  //             0: { items: 2.5, nav: false },
+  //             600: { items: 3, nav: false },
+  //             1000: { items: 5, nav: false }
+  //           }
+  //         });
 
         
-    })
+  //   })
 
 
-  }  upcomingMovies()
+  // }  upcomingMovies()
 
 
 
 
-  function topRatedMovies (){
+  // function topRatedMovies (){
 
 
-    fetch(`https://api.themoviedb.org/3/movie/top_rated?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
-    .then(res => res.json())
-    .then(data => {
-        const movies = data.results;
-        // mainMoviesStorage = [mainMoviesStorage, movies]
-        // console.log(mainMoviesStorage);
+  //   fetch(`https://api.themoviedb.org/3/movie/top_rated?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //       const movies = data.results;
+  //       // mainMoviesStorage = [mainMoviesStorage, movies]
+  //       // console.log(mainMoviesStorage);
 
 
-        $('.owl-carousel').trigger('destroy.owl.carousel');
+  //       $('.owl-carousel').trigger('destroy.owl.carousel');
       
 
-        movies.forEach(movie => {
-            if (movie.poster_path) {
-              topRated.innerHTML += `
-                <div class="movie item">
-                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-                </div>
-              `;
-            }else {
-              console.log('dddddddddddddddddddddddddddddddddddd');
+  //       movies.forEach(movie => {
+  //           if (movie.poster_path) {
+  //             topRated.innerHTML += `
+  //               <div class="movie item">
+  //                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+  //               </div>
+  //             `;
+  //           }else {
+  //             console.log('dddddddddddddddddddddddddddddddddddd');
               
-            }
-          });
+  //           }
+  //         });
 
 
-          $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            responsiveClass: true,
-            responsive: {
-              0: { items: 2.5, nav: false },
-              600: { items: 3, nav: false },
-              1000: { items: 5, nav: false }
-            }
-          });
+  //         $('.owl-carousel').owlCarousel({
+  //           loop: true,
+  //           margin: 10,
+  //           responsiveClass: true,
+  //           responsive: {
+  //             0: { items: 2.5, nav: false },
+  //             600: { items: 3, nav: false },
+  //             1000: { items: 5, nav: false }
+  //           }
+  //         });
 
         
-    })
+  //   })
 
 
-  }  topRatedMovies()
+  // }  topRatedMovies()
 
 
 
 
-  function nowPlayingMovies (){
+  // function nowPlayingMovies (){
 
 
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
-    .then(res => res.json())
-    .then(data => {
-        const movies = data.results;
-        // mainMoviesStorage = [mainMoviesStorage, movies]
-        // console.log(mainMoviesStorage);
+  //   fetch(`https://api.themoviedb.org/3/movie/now_playing?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //       const movies = data.results;
+  //       // mainMoviesStorage = [mainMoviesStorage, movies]
+  //       // console.log(mainMoviesStorage);
 
 
-        $('.owl-carousel').trigger('destroy.owl.carousel');
-        // $('.owl-carousel').html(''); 
+  //       $('.owl-carousel').trigger('destroy.owl.carousel');
+  //       // $('.owl-carousel').html(''); 
      
         
 
-        movies.forEach(movie => {
-            if (movie.poster_path) {
-              nowPlaying.innerHTML += `
-                <div class="movie item">
-                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-                </div>
-              `;
-            }else {
-              console.log('dddddddddddddddddddddddddddddddddddd');
+  //       movies.forEach(movie => {
+  //           if (movie.poster_path) {
+  //             nowPlaying.innerHTML += `
+  //               <div class="movie item">
+  //                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+  //               </div>
+  //             `;
+  //           }else {
+  //             console.log('dddddddddddddddddddddddddddddddddddd');
               
-            }
-          });
+  //           }
+  //         });
 
 
-          $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            responsiveClass: true,
-            responsive: {
-              0: { items: 2.5, nav: false },
-              600: { items: 3, nav: false },
-              1000: { items: 5, nav: false }
-            }
-          });
+  //         $('.owl-carousel').owlCarousel({
+  //           loop: true,
+  //           margin: 10,
+  //           responsiveClass: true,
+  //           responsive: {
+  //             0: { items: 2.5, nav: false },
+  //             600: { items: 3, nav: false },
+  //             1000: { items: 5, nav: false }
+  //           }
+  //         });
 
         
-    })
+  //   })
 
 
-  }  nowPlayingMovies()
+  // }  nowPlayingMovies()
 
 
 
